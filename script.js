2639ae1bc7fd4f43d2b12bc4703faeb1194cf530
Bojan Angelov <67283864+AngelovBojan@users.noreply.github.com>
2021-09-02 11:54:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function toggle_light_mode() {
+    var app = document.getElementsByTagName("BODY")[0];
+    if (localStorage.lightMode == "dark") {
+        localStorage.lightMode = "light";
+        app.setAttribute("light-mode", "light");
+    } else {
+        localStorage.lightMode = "dark";
+        app.setAttribute("light-mode", "dark");
+    }
+
+    if($(".image-landing-page").attr('src') != "file:///C:/Web%20Development/Freelance/Upwork/Digital-Agency(ULTRASOFT)/Images/urn_aaid_sc_US_142398be-60e1-4fe3-b17e-75d32ddb1379.png"){
+        $(".image-landing-page").attr("src","file:///C:/Web%20Development/Freelance/Upwork/Digital-Agency(ULTRASOFT)/Images/urn_aaid_sc_US_142398be-60e1-4fe3-b17e-75d32ddb1379.png");
+      } else {
+        $(".image-landing-page").attr("src","file:///C:/Web%20Development/Freelance/Upwork/Digital-Agency(ULTRASOFT)/Images/Group%20120.svg");
+      }
+
+      if($(".navbar-logo").attr('src') != "file:///C:/Web%20Development/Freelance/Upwork/Digital-Agency(ULTRASOFT)/Images/Group%20115.png"){
+        $(".navbar-logo").attr("src","file:///C:/Web%20Development/Freelance/Upwork/Digital-Agency(ULTRASOFT)/Images/Group%20115.png");
+      } else {
+        $(".navbar-logo").attr("src","file:///C:/Web%20Development/Freelance/Upwork/Digital-Agency(ULTRASOFT)/Images/Group%20115-dark-mode.png");
+      }
+}
+
+window.addEventListener(
+    "storage",
+    function () {
+        if (localStorage.lightMode == "dark") {
+            app.setAttribute("light-mode", "dark");
+        } else {
+            app.setAttribute("light-mode", "light");
+        }
+    },
+    false
+);
+
